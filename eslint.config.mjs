990a9f875b17eec9733c79ab0d0c91427a3ec1a8
teifangu <ifaneds1@gmail.com>
2026-02-04 19:30:49 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import nextVitals from 'eslint-config-next/core-web-vitals'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+      'react-hooks/immutability': 'warn',
+    },
+  },
   globalIgnores([
     '.next/**',
     'out/**',
