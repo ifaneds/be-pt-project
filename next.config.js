e2a62096c19f959289b328604ac3ea44551573ab
teifangu <ifaneds1@gmail.com>
2026-02-04 18:51:05 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const basePath = process.env.BASE_PATH || ''
 
 const nextConfig = {
   output: 'export',
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
+  basePath: basePath,
+  assetPrefix: basePath ? basePath + '/' : undefined,
   images: {
     unoptimized: true, // required for static export on GitHub Pages
     remotePatterns: [
